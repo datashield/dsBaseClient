7972bf0cfacff4e48a73a732ecc20412016a3ff9
agaye <ag13748@it017324.(none)>
2014-02-26 13:15:19 +0000

    Re-commented the code of the function ds.checkvar.

--- R/ds.checkvar.R
+++ R/ds.checkvar.R
@@ -57,77 +57,77 @@ ds.checkvar <- function(datasources=NULL, variables=NULL){
   }
   
   # print a message for the user informing of checks
-  message("\nChecks are carried out on the variables used for the analysis\nto ensure they are available from the dataset(s) and not empty.\n")
-  
-  # names of the studies
-  stdnames <- names(datasources)
-  
-  # global checks results holder
-  track <- c()
-  
-  # loop through the input variables and check each in all the datasources
-  for(i in 1:length(variables)){
-    
-    # get the variable and check if it is given in the form 'D$name'
-    var2check <- variables[[i]]
-    inputterm <- unlist(strsplit(deparse(var2check), "\\$", perl=TRUE))
-    
-    # a vector that keeps the results of the checks for each study
-    toremove <- c()
-    
-    if(length(inputterm) > 1){
-      # name of the variable
-      varname <- strsplit(deparse(var2check), "\\$", perl=TRUE)[[1]][2]
-      
-      # call the function that checks if the variable is present in the assigned dataset
-      presence <- ds.isPresent(datasources, var2check)
-      
-      # call the function that checks if the variable is present in the assigned dataset
-      emptyness <- ds.isNA(datasources, var2check)
-      
-      for(i in 1:length(datasources)){
-        if( !(presence[[i]]) || (emptyness[[i]])){
-          toremove <- append(toremove, 1)
-        }else{
-          toremove <- append(toremove, 0)
-        }
-      }
-    }else{
-      # name of the variable
-      varname <- deparse(var2check)
-      # if the variable is not given in the form 'D$name' no checks for presence required
-      emptyness <- ds.isNA(datasources, var2check)
-      for(i in 1:length(datasources)){
-        if(emptyness[[i]]){
-          toremove <- append(toremove, 1)
-        }else{
-          toremove <- append(toremove, 0)
-        }
-      }     
-    }
-    
-    # the studies where the checked variable is missing or completly empty 
-    # are removed from the studies to analyse.
-    studies2rm <- which(toremove == 1)
-    if(length(studies2rm) > 0){
-      track <- append(track, studies2rm)
-    }else{
-      #message("checks went fine for ", varname)
-    }
-  }
-  
-  # check final results and display final message
-  final.res <- unique(track)
-  if(!(is.null(final.res))){
-    #datasources <- datasources[-final.res] 
-    if(length(datasources) == 0 ){
-      #stop("\nAll the studies failed the checks for one or more variables!\nTHE ANALYSIS CANNOT BE CARRIED OUT!\n")
-    }else{
-      #warning(paste(stdnames[final.res]), " will not be included in the analysis!\n")
-    }
-  }else{
-   # message("All the variables are available and none has 'NA' at all its entries!\n")
-  }
+#   message("\nChecks are carried out on the variables used for the analysis\nto ensure they are available from the dataset(s) and not empty.\n")
+#   
+#   # names of the studies
+#   stdnames <- names(datasources)
+#   
+#   # global checks results holder
+#   track <- c()
+#   
+#   # loop through the input variables and check each in all the datasources
+#   for(i in 1:length(variables)){
+#     
+#     # get the variable and check if it is given in the form 'D$name'
+#     var2check <- variables[[i]]
+#     inputterm <- unlist(strsplit(deparse(var2check), "\\$", perl=TRUE))
+#     
+#     # a vector that keeps the results of the checks for each study
+#     toremove <- c()
+#     
+#     if(length(inputterm) > 1){
+#       # name of the variable
+#       varname <- strsplit(deparse(var2check), "\\$", perl=TRUE)[[1]][2]
+#       
+#       # call the function that checks if the variable is present in the assigned dataset
+#       presence <- ds.isPresent(datasources, var2check)
+#       
+#       # call the function that checks if the variable is present in the assigned dataset
+#       emptyness <- ds.isNA(datasources, var2check)
+#       
+#       for(i in 1:length(datasources)){
+#         if( !(presence[[i]]) || (emptyness[[i]])){
+#           toremove <- append(toremove, 1)
+#         }else{
+#           toremove <- append(toremove, 0)
+#         }
+#       }
+#     }else{
+#       # name of the variable
+#       varname <- deparse(var2check)
+#       # if the variable is not given in the form 'D$name' no checks for presence required
+#       emptyness <- ds.isNA(datasources, var2check)
+#       for(i in 1:length(datasources)){
+#         if(emptyness[[i]]){
+#           toremove <- append(toremove, 1)
+#         }else{
+#           toremove <- append(toremove, 0)
+#         }
+#       }     
+#     }
+#     
+#     # the studies where the checked variable is missing or completly empty 
+#     # are removed from the studies to analyse.
+#     studies2rm <- which(toremove == 1)
+#     if(length(studies2rm) > 0){
+#       track <- append(track, studies2rm)
+#     }else{
+#       #message("checks went fine for ", varname)
+#     }
+#   }
+#   
+#   # check final results and display final message
+#   final.res <- unique(track)
+#   if(!(is.null(final.res))){
+#     #datasources <- datasources[-final.res] 
+#     if(length(datasources) == 0 ){
+#       #stop("\nAll the studies failed the checks for one or more variables!\nTHE ANALYSIS CANNOT BE CARRIED OUT!\n")
+#     }else{
+#       #warning(paste(stdnames[final.res]), " will not be included in the analysis!\n")
+#     }
+#   }else{
+#    # message("All the variables are available and none has 'NA' at all its entries!\n")
+#   }
   return(datasources)
 }
 
