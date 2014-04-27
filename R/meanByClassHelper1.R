5ea113efc096ef7380af09ee1eca4c0808a78536
agaye <ag13748@it017324.(none)>
2014-04-27 16:53:26 +0100

    Type corrected.

--- R/meanByClassHelper1.R
+++ R/meanByClassHelper1.R
@@ -27,7 +27,7 @@
     if(length(check1) > 0 | length(check2) > 0){ 
       newtablenames <- append(newtablenames, dsbaseclient:::.meanByClassHelper4(dtsource, paste0('holder',i), dt2process, variable, categories))
     }else{
-      ds.subclass(dtsource, paste0('holder',i), tables[i], variable)
+      ds.subclass(dtsource, paste0('holder',i), dt2process, variable)
       newtablenames <- append(newtablenames, dsbaseclient:::.meanByClassHelper4(dtsource, paste0('holder',i), dt2process))
     }
   }
