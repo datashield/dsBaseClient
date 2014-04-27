4ef31a4a3c652b8bd098d7a7f64605380fe4d56f
agaye <ag13748@it017324.(none)>
2014-04-27 16:39:33 +0100

    Bug sorted.

--- R/meanByClassHelper1.R
+++ R/meanByClassHelper1.R
@@ -24,7 +24,7 @@
 
     check1 <- which(unlist(strsplit(dt2process,"_")) == "INVALID")
     check2 <- which(unlist(strsplit(dt2process,"_")) == "EMPTY")
-    if(check0 > 0 | length(check1) > 0 | length(check2) > 0){ 
+    if(length(check1) > 0 | length(check2) > 0){ 
       newtablenames <- append(newtablenames, dsbaseclient:::.meanByClassHelper4(dtsource, paste0('holder',i), dt2process, variable, categories))
     }else{
       ds.subclass(dtsource, paste0('holder',i), tables[i], variable)
