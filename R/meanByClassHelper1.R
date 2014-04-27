e17b3f72c6a2a0835b0e988e574af8077081b4b3
agaye <ag13748@it017324.(none)>
2014-04-27 15:29:20 +0100

    A test inserted to check if the vector to subset by is empty.

--- R/meanByClassHelper1.R
+++ R/meanByClassHelper1.R
@@ -13,9 +13,13 @@
   
   newtablenames <- c()
   for(i in 1:length(tables)){
+    # check if the vector to subset by is not empty(i.e. NAs only) as this might cause ds.subclass to crash
+    ds.asNumeric(dtsource, paste0(tables[i], "$", variable), 'vectnumeric')
+    ch0 <- unlist(datashield.aggregate(dtsource, quote(isNA.ds(vectnumeric))))
+    if(ch0){ check0 <- 1}else{ check0 <- 0}
     check1 <- which(unlist(strsplit(tables[i],"_")) == "INVALID")
     check2 <- which(unlist(strsplit(tables[i],"_")) == "EMPTY")
-    if(length(check1) > 0 | length(check2) > 0){ 
+    if(check0 > 0 | length(check1) > 0 | length(check2) > 0){ 
       newtablenames <- append(newtablenames, dsbaseclient:::.meanByClassHelper4(dtsource, paste0('holder',i), tables[i], variable, categories))
     }else{
       ds.subclass(dtsource, paste0('holder',i), tables[i], variable)
