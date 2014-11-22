36371716534cdc1c0420e5d9fe42e83fef65521c
agaye <ag13748@it017324.(none)>
2014-11-22 23:40:07 +0000

    Functions 'ds.cbind' and 'ds.replaceNA' amended.

--- R/ds.cbind.R
+++ R/ds.cbind.R
@@ -1,11 +1,11 @@
 #' 
 #' @title Combines objects by columns
-#' @description this is similar to the R base function 'cbind'
-#' @details the function combines vectors or vectors and matrices or dataframes by columns.
-#' But unlike the R base function 'cbind' the output is a dataframe.
-#' @param x a vector which holds the names of the objects to combine
+#' @description This is similar to the R base function \code{cbind} with the only differences 
+#' that  it allows to combine up to 5 objects (vectors and/or table structures).
+#' @details see details of the R base function \code{cbind}.
+#' @param x a character vector, the name of the vector and or table to combine by column.
 #' @param newobj the name of the output object. If this argument is set to \code{NULL}, 
-#' the name of the new object is 'newDataframe'.
+#' the name of the new object is 'newCbindObject'.
 #' @param datasources a list of opal object(s) obtained after login in to opal servers;
 #' these objects hold also the data assign to R, as \code{dataframe}, from opal datasources.
 #' @return nothing is returned to the client, the new object is stored on the server side.
@@ -21,18 +21,18 @@
 #' myvar <- list('LAB_TSC', 'LAB_HDL')
 #' opals <- datashield.login(logins=logindata,assign=TRUE,variables=myvar)
 #' 
-#' # generate a new dataframe by combining the log values of 'LAB_TSC' and 'LAB_HDL', by columns
+#' # generate a new dataframe by combining the log values of 
+#' # 'LAB_TSC' and 'LAB_HDL', by columns
 #' ds.assign(toAssign='log(D$LAB_TSC)', newobj='labtsc')
 #' ds.assign(toAssign='log(D$LAB_HDL)', newobj='labhdl')
-#' myobjects <- c('labtsc', 'labhdl')
-#' ds.cbind(x=myobjects)
+#' ds.cbind(x=c('labtsc','labhdl'), newobj="myDataframe")
 #' 
 #' # clear the Datashield R sessions and logout
 #' datashield.logout(opals)
 #' 
 #' }
 #' 
-ds.cbind = function(x=NULL, newobj=NULL, datasources=NULL){
+ds.cbind <- function(x=NULL, newobj='newCbindObject', datasources=NULL){
 
   # if no opal login details were provided look for 'opal' objects in the environment 
   if(is.null(datasources)){
@@ -53,43 +53,31 @@ ds.cbind = function(x=NULL, newobj=NULL, datasources=NULL){
     }
   }
   
-  if(is.null(x)){
-    stop("x=NULL. Please provide the names of the objects to combine!", call.=FALSE)
-  }
-  
+  # if not more than one input objects stop 
   if(length(x) < 2){
-    stop("You must provide the names of at least two objects!\n", call.=FALSE)
-  }
-  
-  # the input variable might be given as column table (i.e. D$object)
-  # or just as a vector not attached to a table (i.e. object)
-  # we have to make sure the function deals with each case
-  xnames <- extract(x)
-  varnames <- xnames$elements
-  obj2lookfor <- xnames$holders
+    stop("You must provide the names of at least two objects!", call.=FALSE)
+  }  
   
   # check if the input object(s) is(are) defined in all the studies
-  for(i in 1:length(varnames)){
-    if(is.na(obj2lookfor[i])){
-      defined <- isDefined(datasources, varnames[i])
-    }else{
-      defined <- isDefined(datasources, obj2lookfor[i])
+  for(i in 1:length(x)){
+    if(!(is.null(x[i]))){
+      inputElts <- extract(x[i])
+      if(is.na(inputElts[[1]])){
+        defined <- isDefined(datasources, inputElts[[2]])
+      }else{
+        defined <- isDefined(datasources, inputElts[[1]])
+        cally <- paste0("colnames(", inputElts[[1]], ")")
+        column_names <- unique(unlist(datashield.aggregate(datasources, cally)))
+        if(!(inputElts[[2]] %in% column_names)){
+          stop(paste0("No variable ",inputElts[[2]]," in ", inputElts[[1]], " in one or more studies"), call.=FALSE)
+        }
+      } 
     }
   }
   
-  # call the internal function that checks the input object(s) is(are) of the same class in all studies.
-  for(i in 1:length(x)){
-    typ <- checkClass(datasources, x[i])
-  }
-    
-  if(is.null(newobj)){
-    newobj <- 'newDataframe'
-  }
-  
   # call the server side function
-  cally <-  paste0("cbindDS(list(",paste(x,collapse=","),")", 
-                   ",list(","'",paste(varnames,collapse="','"),"'","))")
-  datashield.assign(datasources, newobj, as.symbol(cally))
+  cally <-  paste0("cbind(", paste(x,collapse=","), ")")
+  datashield.assign(datasources, newobj, as.symbol(cally)) 
   
   # check that the new object has been created and display a message accordingly
   finalcheck <- isAssigned(datasources, newobj)
--- R/ds.replaceNA.R
+++ R/ds.replaceNA.R
@@ -7,16 +7,17 @@
 #' the number of missing values using the function \code{ds.numNA} but in most cases
 #' it might be more sensible to replace all missing values by one specific value e.g. 
 #' replace all missing values in a vector by the mean or median value. Once the missing
-#' values have been replaced a new vector is created. If the vector is within a table 
-#' structure such as a data frame a new data frame is generated with the new vector.
+#' values have been replaced a new vector is created. 
+#' NOTE: If the vector is within a table structure such as a data frame the new vector is 
+#' appended to table structure so that the table hold hold both the vector with and without
+#' missing values. The latter is, by default, given a different that indicates its 'completeness'. 
 #' @param x a character, the name of the vector to process.
 #' @param forNA a list which contains the replacement value(s), a vector one or more values 
 #' for each study. The length of the list must be equal to the number of servers the analyst 
 #' is connected to. 
-#' @param newobj a character, the name of the new vector or table structure in which missing 
-#' values have been replaced. If no name is specified the default name is the name of the original 
-#' vector or table structure followed by the suffix '_noMissings' e.g. 'LAB_HDL_noNA' or 
-#' 'D_LAB_HDL_noNA' if 'LAB_HDL' is in table 'D'.
+#' @param newobj a character, the name of the new vector in which missing values have been replaced. 
+#' If no name is specified the default name is the name of the original vector followed by the suffix 
+#' '.noNA' e.g. 'LAB_HDL.noNA' if the name of the vector is 'LAB_HDL'.
 #' @param datasources a list of opal object(s) obtained after login in to opal servers;
 #' these objects hold also the data assign to R, as \code{dataframe}, from opal datasources.
 #' @return a new vector or table structure with the same class is stored on the server site.
@@ -33,8 +34,10 @@
 #' # Replace missing values in variable 'LAB_HDL' by the mean value in each study
 #' # first let us get the mean value for 'LAB_HDL' in each study
 #' ds.mean(x='D$LAB_HDL', type='split')
-#' # now do the repalcement
-#' ds.repalceNA(x='D$LAB_HDL', forNA=list(1.569416, 1.556648), newobj='D_HDL_noNA')
+#' 
+#' # replace missing values in the variable 'LAB_HDL' in dataf frame 'D' by 
+#' # the mean value and name the new variable 'HDL.noNA'. 
+#' ds.replaceNA(x='D$LAB_HDL', forNA=list(1.569416, 1.556648), newobj='HDL.noNA')
 #' 
 #' # clear the Datashield R sessions and logout
 #' datashield.logout(opals)
@@ -70,7 +73,7 @@ ds.replaceNA = function(x=NULL, forNA=NULL, newobj=NULL, datasources=NULL) {
   if(is.null(forNA)){
     stop("Please provide a list of replacement values!", call.=FALSE)
   }else{
-    if(length(forNA != length(datasources))){
+    if(length(forNA) != length(datasources)){
       stop("'forNA' must be of the same length as the number of datasources/studies!", call.=FALSE)
     }
   }
@@ -88,29 +91,31 @@ ds.replaceNA = function(x=NULL, forNA=NULL, newobj=NULL, datasources=NULL) {
     # replacement values is not 1 and is greater or smaller than the actual 
     # number of missing values stop the process and tell the analyst
     cally <- paste0("numNaDS(", x, ")")
-    numNAs <-unlist(datashield.aggregate(datasources[i], as.symbol(cally)))  
-    if(numNAs != 1 & numNAs != forNA[[i]]){
+    numNAs <- datashield.aggregate(datasources[i], as.symbol(cally)) 
+    if(length(forNA[[i]]) != 1 & length(forNA[[i]]) != numNAs[[1]]){
       message("The number of replacement values must be of length 1 or of the same length as the number of missing values.")
       stop(paste0("This is not the case in ", names(datasources)[i]), call.=FALSE)
     }
   }
   
   if(is.null(newobj)){
-    if(!(is.na(inputElts[[1]]))){
-      newobj <- paste0(inputElts[[1]],"_",inputElts[[2]],"_noNA")
-    }else{
-      newobj <- paste0(inputElts[[2]],"_noNA")
-    }
+    newobj <- paste0(inputElts[[2]],".noNA")
   }
   
   # call the server side function and doo the replacement for each server
   for(i in 1:length(datasources)){
     message(paste0("--Processing ", names(datasources)[i], "..."))
-    cally <- call("replaceNaDS", as.symbol(x), forNA[[i]], newobj)
-    numNAs <- datashield.aggregate(datasources, as.symbol(cally))
-    
+    cally <- paste0("replaceNaDS(", x, paste0(", c(",paste(forNA[[i]],collapse=","),")"), ")")
+    datashield.assign(datasources[i], newobj, as.symbol(cally))
+
     # check that the new object has been created and display a message accordingly
-    finalcheck <- isAssigned(datasources, newobj) 
+    finalcheck <- isAssigned(datasources[i], newobj) 
+    
+    # if the input vector is within a table structure append the new vector to that table
+    if(!(is.na(inputElts[[1]]))){
+      cally <-  paste0("cbind(", inputElts[[1]], ",", newobj, ")")
+      datashield.assign(datasources[i], inputElts[[1]], as.symbol(cally)) 
+    }
   }
   
 }
