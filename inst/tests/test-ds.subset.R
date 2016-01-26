7aa6d2bd06c8e596f4a243c9c79faeb6c8f6a851
Neil Parley <mail@neilparley.co.uk>
2016-01-26 13:55:39 +0000

    Update subset test

--- inst/tests/test-ds.subset.R
+++ inst/tests/test-ds.subset.R
@@ -12,7 +12,7 @@
 # Set up
 #
 
-context("dsbaseclient::ds.subset")
+context("dsBaseClient::ds.subset")
 
 options(datashield.variables=list("DIS_DIAB","PM_BMI_CONTINUOUS","LAB_HDL", "GENDER"))
 source("setup.R")
@@ -21,27 +21,52 @@ source("setup.R")
 # Tests
 #
 
-context("dsbaseclient::ds.subset() generate a subset of the assigned table (by default the table is named 'D') with the first 50 observations and the two first columns")
-ds.subset(datasources=opals, subset='subD', data='D', rows=c(1:50), cols=c(1,2))
-# TODO do more than a smoke test
+context("dsBaseClient::ds.subset() generate a subset of the assigned table (by default the table is named 'D') with the first 50 observations and the two first columns")
+ds.subset(datasources=opals, subset='subD', x='D', rows=c(1:50), cols=c(1,2))
+res <- ds.exists('subD')
+test_that("subD_exists", {
+    expect_true(res$sim1)
+    expect_true(res$sim2)
+    expect_true(res$sim3)
+})
 
-context("dsbaseclient::ds.subset() generate a subset of the assigned table (by default the table is named 'D') with the first 50 observations and the two first columns refered to by their names")
-ds.subset(datasources=opals, subset='subD', data='D', rows=c(1:50), cols <- c('DIS_DIAB','PM_BMI_CONTINUOUS'))
-# TODO do more than a smoke test
+context("dsBaseClient::ds.subset() generate a subset of the assigned table (by default the table is named 'D') with the first 50 observations and the two first columns refered to by their names")
+ds.subset(datasources=opals, subset='subD2', x='D', rows=c(1:50), cols = c('DIS_DIAB','PM_BMI_CONTINUOUS'))
+res <- ds.exists('subD2')
+test_that("subD2_exists", {
+    expect_true(res$sim1)
+    expect_true(res$sim2)
+    expect_true(res$sim3)
+})
 
-context("dsbaseclient::ds.subset() generate a subset of the table D with bmi values greater than or equal to 25.")
-ds.subset(datasources=opals, subset='subD', data='D', logical='PM_BMI_CONTINUOUS>=', threshold=25)
-# TODO do more than a smoke test
+context("dsBaseClient::ds.subset() generate a subset of the table D with bmi values greater than or equal to 25.")
+ds.subset(datasources=opals, subset='subD3', x='D', logical='PM_BMI_CONTINUOUS>=', threshold=25)
+res <- ds.exists('subD3')
+test_that("subD3_exists", {
+    expect_true(res$sim1)
+    expect_true(res$sim2)
+    expect_true(res$sim3)
+})
 
-context("dsbaseclient::ds.subset() get the logarithmic values of the variable 'lab_hdl' and generate a subset with the first 50 observations of that new vector.")
-ds.assign(opals, "logHDL", "log(D$LAB_HDL)")
-ds.subset(datasources=opals, subset="subLAB_HDL", data="logHDL", rows=c(1:50))
-# TODO do more than a smoke test
+context("dsBaseClient::ds.subset() get the logarithmic values of the variable 'lab_hdl' and generate a subset with the first 50 observations of that new vector.")
+ds.assign(toAssign='log(D$LAB_HDL)', newobj='logHDL')
+ds.subset(datasources=opals, subset="subLAB_HDL", x="logHDL", rows=c(1:50))
+res <- ds.exists('subLAB_HDL')
+test_that("subLAB_HDL_exists", {
+    expect_true(res$sim1)
+    expect_true(res$sim2)
+    expect_true(res$sim3)
+})
 
-context("dsbaseclient::ds.subset() get the variable 'PM_BMI_CONTINUOUS' from the dataframe 'D' and generate a subset bmi vector with bmi values greater than or equal to 25")
-ds.assign(opals, "BMI", "D$PM_BMI_CONTINUOUS")
-ds.subset(datasources=opals, subset='subBMI', data='BMI', logical='>=', threshold=25)
-# TODO do more than a smoke test
+context("dsBaseClient::ds.subset() get the variable 'PM_BMI_CONTINUOUS' from the dataframe 'D' and generate a subset bmi vector with bmi values greater than or equal to 25")
+ds.assign(toAssign='D$PM_BMI_CONTINUOUS', newobj='BMI')
+ds.subset(datasources=opals, subset='subBMI', x='BMI', logical='>=', threshold=25)
+res <- ds.exists('subBMI')
+test_that("subBMI_exists", {
+    expect_true(res$sim1)
+    expect_true(res$sim2)
+    expect_true(res$sim3)
+})
 
 #
 # Tear down
