8efa4f44ff2e63bbecf6dda9c812840aa77302f1
Neil Parley <mail@neilparley.co.uk>
2016-01-28 15:51:30 +0000

    Add tests for ds.summary

---
+++ tests/testthat/test-ds.summary.R
@@ -0,0 +1,55 @@
+context("dsBaseClient::ds.summary")
+
+source("setup.R")
+#
+# Tests
+#
+
+
+context("dsBaseClient::ds.summary: suummary of a numerical variable")
+
+res <- ds.summary(x='D$LAB_TSC')
+test_that("summary_numerical_variable", {
+  expect_equal(res$sim1$class, "numeric")
+  expect_equal(res$sim2$length, 3088)
+  expect_equal(res$sim3$`quantiles & mean`[[4]], 5.786)
+})
+
+context("dsBaseClient::ds.summary: suummary of a character variable")
+ds.asCharacter(x='D$GENDER', newobj="a_character")
+res <- ds.summary(x='a_character')
+test_that("summary_character_variable", {
+  expect_equal(res$sim1$class, "character")
+  expect_equal(res$sim2$length, 3088)
+})
+
+context("dsBaseClient::ds.summary: suummary of a factor variable")
+res <- ds.summary(x='D$GENDER')
+test_that("summary_factor_variable", {
+  expect_equal(res$sim1$class, "factor")
+  expect_equal(res$sim2$length, 3088)
+  expect_equal(res$sim3$`count of '0'`, 2091)
+  expect_equal(res$sim1$categories[[1]], "0")
+  expect_equal(res$sim1$categories[[2]], "1")
+})
+
+context("dsBaseClient::ds.summary: suummary of a data frame")
+res <- ds.summary(x='D')
+test_that("summary_data_frame", {
+  expect_equal(res$sim1$class, "data.frame")
+  expect_equal(res$sim2$`number of rows`, 3088)
+  expect_equal(res$sim2$`number of columns`, 11)
+  expect_equal(res$sim3$`variables held`[[11]], "PM_BMI_CATEGORICAL")
+  expect_equal(res$sim1$`variables held`[[2]], "LAB_TRIG")
+})
+
+context("dsBaseClient::ds.summary() test errors")
+ds.asCharacter(x='D$LAB_TSC', newobj="not_a_numeric")
+test_that("summary_erros", {
+    expect_error(ds.summary(), "Please provide the name of the input vector!", fixed=TRUE)
+})
+#
+# Tear down
+#
+
+source("teardown.R")
