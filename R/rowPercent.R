55f778c43e37e0cb190757d7c6ab66375ba9b666
Demetris Avraam <avraam.demetris@gmail.com>
2019-03-09 13:18:44 +0200

    Update rowPercent.R

--- R/rowPercent.R
+++ R/rowPercent.R
@@ -1,13 +1,14 @@
 #'
 #' @title Produces row percentages
 #' @description this is an INTERNAL function.
-#' @details The function is required required by the client function \code{ds.table2D}. 
+#' @details The function is required by the client function \code{ds.table2D}. 
 #' @param dataframe a data frame
 #' @keywords internal
 #' @return a data frame
 #' @author Gaye, A.
 #' 
 rowPercent <- function(dataframe){
+  
   dt <- dataframe
   lastrow <- dim(dataframe)[1]
   lastcol <- dim(dataframe)[2]
@@ -15,8 +16,7 @@ rowPercent <- function(dataframe){
     totalval <- dataframe[i, lastcol]
     dt[i,] <- round((dataframe[i,]/totalval)*100,2)      
   }
+  
   return(dt)
+  
 }
-
-
-
