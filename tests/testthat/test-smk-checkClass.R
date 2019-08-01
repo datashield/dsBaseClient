7b5c826445cf844a039be473745b9929d6e5d765
Stuart Wheater <stuart.wheater@arjuna.com>
2019-08-01 22:05:36 +0100

    Transfer from 'dsBetaTestClient' and remove '.o'

---
+++ tests/testthat/test-smk-checkClass.R
@@ -0,0 +1,92 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2014 OBiBa,
+#               2018 University of Newcastle upon Tyne. All rights reserved.
+#
+# This program and the accompanying materials
+# are made available under the terms of the GNU Public License v3.0.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#-------------------------------------------------------------------------------
+
+#
+# Set up
+#
+
+# context("dsBetaTestClient::checkClass::smk")
+
+source("connection_to_datasets/init_all_datasets.R")
+source("connection_to_datasets/init_smk_datasets.R")
+
+connect.smk.dataset.sim(list("GENDER", "LAB_TSC", "LAB_TRIG", "LAB_HDL", "DIS_CVA", "DIS_AMI"))
+
+#
+# Tests
+#
+
+context("checkClass::smk::simple test")
+
+test_that("simple test", {
+    res <- checkClass(ds.test_env$connection.opal, "D$GENDER")
+
+    expect_length(res, 1)
+    expect_equal(class(res), "character")
+    expect_equal(res, "factor")
+})
+
+test_that("simple test", {
+    res <- checkClass(ds.test_env$connection.opal, "D$LAB_TSC")
+
+    expect_length(res, 1)
+    expect_equal(class(res), "character")
+    expect_equal(res, "numeric")
+})
+
+test_that("simple test", {
+    res <- checkClass(ds.test_env$connection.opal, "D$LAB_TRIG")
+
+    expect_length(res, 1)
+    expect_equal(class(res), "character")
+    expect_equal(res, "numeric")
+})
+
+test_that("simple test", {
+    res <- checkClass(ds.test_env$connection.opal, "D$LAB_HDL")
+
+    expect_length(res, 1)
+    expect_equal(class(res), "character")
+    expect_equal(res, "numeric")
+})
+
+test_that("simple test", {
+    res <- checkClass(ds.test_env$connection.opal, "D$DIS_CVA")
+
+    expect_length(res, 1)
+    expect_equal(class(res), "character")
+    expect_equal(res, "factor")
+})
+
+test_that("simple test", {
+    res <- checkClass(ds.test_env$connection.opal, "D$DIS_AMI")
+
+    expect_length(res, 1)
+    expect_equal(class(res), "character")
+    expect_equal(res, "factor")
+})
+
+test_that("data.frame test", {
+    myvectors <- c('D$LAB_TSC', 'D$LAB_HDL')
+    ds.dataframe(x=myvectors)
+
+    res <- checkClass(ds.test_env$connection.opal, "df_new")
+
+    expect_length(res, 1)
+    expect_equal(class(res), "character")
+    expect_equal(res, "data.frame")
+})
+
+#
+# Done
+#
+
+# context("dsBetaTestClient::checkClass 1::smk done")
