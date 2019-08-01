ef20f9f8ee12df16410f5dd54d1cb5be590a53b6
Stuart Wheater <stuart.wheater@arjuna.com>
2019-08-01 20:17:06 +0100

    Transfer from 'dsBetaTestClient' and remove '.o'

---
+++ tests/testthat/test-arg-ds.asFactor.R
@@ -0,0 +1,38 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2014 OBiBa,
+#               2018 University of Newcastle upon Tyne. All rights reserved.
+#
+# This program and the accompanying materials
+# are made available under the terms of the GNU Public License v3.0.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#-------------------------------------------------------------------------------
+
+#
+# Set up
+#
+
+# context("dsBetaTestClient::ds.asFactor:args test")
+
+source("connection_to_datasets/init_all_datasets.R")
+source("connection_to_datasets/init_smk_datasets.R")
+
+connect.smk.dataset.sim(list("LAB_TSC"))
+
+#
+# Tests
+#
+
+context("ds.asFactor::arg::test errors")
+test_that("asFactor_erros", {
+    expect_error(ds.asFactor(), "Please provide the name of the variable that is to be converted to a factor e.g. 'varname'", fixed=TRUE)
+    expect_error(ds.asFactor(input.var.name=1), "Please provide the name of the variable that is to be converted to a factor in character format e.g. 'varname'", fixed=TRUE)
+    expect_error(ds.asFactor(input.var.name=NA), "Please provide the name of the variable that is to be converted to a factor in character format e.g. 'varname'", fixed=TRUE)
+})
+
+#
+# Done
+#
+
+# context("dsBetaTestClient::ds.asFactor:arg done")
---
+++ tests/testthat/test-arg-ds.asNumeric.R
@@ -0,0 +1,36 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2014 OBiBa,
+#               2018 University of Newcastle upon Tyne. All rights reserved.
+#
+# This program and the accompanying materials
+# are made available under the terms of the GNU Public License v3.0.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#-------------------------------------------------------------------------------
+
+#
+# Set up
+#
+
+# context("dsBetaTestClient::ds.asNumeric:args test")
+
+source("connection_to_datasets/init_all_datasets.R")
+source("connection_to_datasets/init_smk_datasets.R")
+
+connect.smk.dataset.sim(list("LAB_TSC"))
+
+#
+# Tests
+#
+
+context("ds.asNumeric::arg::test errors")
+test_that("asNumeric_erros", {
+    expect_error(ds.asNumeric(), "Please provide the name of the input vector!", fixed=TRUE)
+})
+
+#
+# Done
+#
+
+# context("dsBetaTestClient::ds.asNumeric:arg done")
