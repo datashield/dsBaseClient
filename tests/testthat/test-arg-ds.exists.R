4b4088a961bb225d9287af566c3f6540e342bfe6
Stuart Wheater <stuart.wheater@arjuna.com>
2019-08-05 22:33:47 +0100

    More smoke and argument tests

---
+++ tests/testthat/test-arg-ds.exists.R
@@ -0,0 +1,32 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2014 OBiBa,
+#               2019 University of Newcastle upon Tyne. All rights reserved.
+#
+# This program and the accompanying materials
+# are made available under the terms of the GNU Public License v3.0.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#-------------------------------------------------------------------------------
+
+#
+# Set up
+#
+
+source("connection_to_datasets/init_all_datasets.R")
+source("connection_to_datasets/init_smk_datasets.R")
+
+connect.smk.dataset.sim(list("LAB_TSC"))
+
+#
+# Tests
+#
+
+context("ds.exists::arg::test errors")
+test_that("simple exists errors", {
+    expect_error(ds.exists(), "Please provide the name of the input object!")
+})
+
+#
+# Done
+#
