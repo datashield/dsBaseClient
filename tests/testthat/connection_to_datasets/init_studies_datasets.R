ec2ab17a27028f9c9801ce9f109c60f240872b7e
Stuart Wheater <stuart.wheater@arjuna.com>
2019-08-12 15:16:10 +0100

    Reworking of test framework

--- tests/testthat/connection_to_datasets/init_studies_datasets.R
+++ tests/testthat/connection_to_datasets/init_studies_datasets.R
@@ -17,6 +17,25 @@ init.studies.dataset.cnsim <- function(variables)
     }
 }
 
+init.studies.dataset.dasim <- function(variables)
+{
+    if (ds.test_env$secure_login_details)
+    {
+        ds.test_env$server <- c("sim1", "sim2", "sim3")
+        ds.test_env$url <- c(ds.test_env$ip_address_1,ds.test_env$ip_address_2,ds.test_env$ip_address_3)
+        ds.test_env$user <- c(ds.test_env$user_1,ds.test_env$user_2,ds.test_env$user_3)
+        ds.test_env$password <- c(ds.test_env$password_1,ds.test_env$password_2,ds.test_env$password_3)
+        ds.test_env$table <- c("DASIM.DASIM1", "DASIM.DASIM2", "DASIM.DASIM3")
+        ds.test_env$login.data <- datashield.build.login.data.frame.o(ds.test_env$server,
+                                                                      ds.test_env$url,
+                                                                      ds.test_env$table,
+                                                                      ds.test_env$user,
+                                                                      ds.test_env$password)
+
+        ds.test_env$stats.var <- variables
+    }
+}
+
 init.studies.dataset.survival <- function(variables)
 {
     if (ds.test_env$secure_login_details)
@@ -44,6 +63,14 @@ connect.studies.dataset.cnsim <- function(variables)
     log.in.data.server()
 }
 
+connect.studies.dataset.dasim <- function(variables)
+{
+    log.out.data.server()
+    source("connection_to_datasets/login_details.R")
+    init.studies.dataset.dasim(variables)
+    log.in.data.server()
+}
+
 connect.studies.dataset.survival <- function(variables)
 {
     log.out.data.server()
@@ -51,3 +78,19 @@ connect.studies.dataset.survival <- function(variables)
     init.studies.dataset.survival(variables)
     log.in.data.server()
 }
+
+disconnect.studies.dataset.cnsim <- function()
+{
+    log.out.data.server()
+}
+
+disconnect.studies.dataset.dasim <- function()
+{
+    log.out.data.server()
+}
+
+disconnect.studies.dataset.survival <- function()
+{
+    log.out.data.server()
+}
+
