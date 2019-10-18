d2e71fb769031dfa773c7cf2ea63da18dad586f0
Stuart Wheater <stuart.wheater@arjuna.com>
2019-10-18 15:26:42 +0100

    Fixed 'testObjExists' response processing

--- R/ds.dataFrameFill.R
+++ R/ds.dataFrameFill.R
@@ -102,7 +102,7 @@ ds.dataFrameFill <- function(df.name=NULL, newobj=NULL, datasources=NULL){
     if(!object.info[[j]]$test.obj.exists){
       obj.name.exists.in.all.sources <- FALSE
     }
-    if(object.info[[j]]$test.obj.class=="ABSENT"){
+    if(is.null(object.info[[j]]$test.obj.class) || object.info[[j]]$test.obj.class=="ABSENT"){
       obj.non.null.in.all.sources <- FALSE
     }
   }
