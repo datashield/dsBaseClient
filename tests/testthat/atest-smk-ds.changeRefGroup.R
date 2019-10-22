04f061ae6e5f514ed39bb230e31534898ff82405
Stuart Wheater <stuart.wheater@arjuna.com>
2019-10-22 13:00:43 +0100

    Initial 'ds.devels' test

---
+++ tests/testthat/atest-smk-ds.changeRefGroup.R
@@ -0,0 +1,45 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2019 University of Newcastle upon Tyne. All rights reserved.
+#
+# This program and the accompanying materials
+# are made available under the terms of the GNU Public License v3.0.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#-------------------------------------------------------------------------------
+
+#
+# Set up
+#
+
+connect.studies.dataset.cnsim(list("DIS_AMI", "GENDER"))
+
+#
+# Tests
+#
+
+context("ds.changeRefGroup::smk")
+test_that("simple changeRefGroup", {
+    
+    print("====")
+    print(ds.colnames("D"))
+    print("====")
+    print(ds.class("D$DIS_AMI"))
+    print("====")
+
+    res <- ds.changeRefGroup("D$DIS_AMI", "D$GENDER")
+
+    expect_length(res, 3)
+    expect_length(res$sim1, 1)
+    expect_equal(res$sim1[1], "D")
+    expect_length(res$sim2, 1)
+    expect_equal(res$sim2[1], "D")
+    expect_length(res$sim3, 1)
+    expect_equal(res$sim3[1], "D")
+})
+
+#
+# Done
+#
+
+disconnect.studies.dataset.cnsim()
