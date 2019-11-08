360aecbc5f52657c6025c25593d8bd57dffd8f87
Stuart Wheater <stuart.wheater@arjuna.com>
2019-11-08 16:22:44 +0000

    Aditional 'ds.message' tests

--- tests/testthat/test-smk-ds.message.R
+++ tests/testthat/test-smk-ds.message.R
@@ -19,13 +19,35 @@ connect.studies.dataset.cnsim(list("LAB_TSC"))
 #
 
 context("ds.message::smk")
-test_that("request message", {
+test_that("not exists - request message", {
     message.res <- ds.message('Test')
 
     expect_length(message.res, 3)
-    expect_equal(message.res$sim1, "Error: the object <message.object.name> does not exist in this datasource")
-    expect_equal(message.res$sim2, "Error: the object <message.object.name> does not exist in this datasource")
-    expect_equal(message.res$sim3, "Error: the object <message.object.name> does not exist in this datasource")
+    expect_equal(message.res$sim1, "Error: the object <message.object.name> does not exist in this datasource", fixed=TRUE)
+    expect_equal(message.res$sim2, "Error: the object <message.object.name> does not exist in this datasource", fixed=TRUE)
+    expect_equal(message.res$sim3, "Error: the object <message.object.name> does not exist in this datasource", fixed=TRUE)
+})
+
+test_that("exists - request message", {
+    ds.list("D$LAB_TSC", "Test")
+
+    message.res <- ds.message('Test')
+
+    expect_length(message.res, 3)
+    expect_equal(message.res$sim1, "ALL OK: there are no studysideMessage(s) on this datasource", fixed=TRUE)
+    expect_equal(message.res$sim2, "ALL OK: there are no studysideMessage(s) on this datasource", fixed=TRUE)
+    expect_equal(message.res$sim3, "ALL OK: there are no studysideMessage(s) on this datasource", fixed=TRUE)
+})
+
+test_that("partial - request message", {
+    ds.list("D$LAB_TSC", newobj="TestP", datasources=ds.test_env$connection.opal[1])
+
+    message.res <- ds.message('TestP')
+
+    expect_length(message.res, 3)
+    expect_equal(message.res$sim1, "ALL OK: there are no studysideMessage(s) on this datasource", fixed=TRUE)
+    expect_equal(message.res$sim2, "Error: the object <message.object.name> does not exist in this datasource", fixed=TRUE)
+    expect_equal(message.res$sim3, "Error: the object <message.object.name> does not exist in this datasource", fixed=TRUE)
 })
 
 #
