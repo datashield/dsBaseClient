416647be7c9cc161adbb4e7543587e99ef5dfdd6
LeireAbarrategui <60744058+LeireAbarrategui@users.noreply.github.com>
2020-03-19 21:36:46 +0000

    Expected value test of ds.dataFrameSort
    This testing have two files:
    - definition of functions (def)
    - call the functions (test-expt)

---
+++ tests/testthat/test-expt-ds.dataFrameSort.R
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2019-2020 University of Newcastle upon Tyne. All rights reserved.
+#
+# This program and the accompanying materials
+# are made available under the terms of the GNU Public License v3.0.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#-------------------------------------------------------------------------------
+
+source('connection_to_datasets/init_testing_datasets.R')
+source('definition_tests/def-ds.dataFrameSort.R')
+
+###!!! test the data frame creation
+
+
+
+context("ds.dataFrameSort()::expt::numeric::increasing")
+test_that("combined data set",
+{
+  connect.all.datasets()
+  .sort.numeric.increasing("D","INTEGER")
+})
+
+
