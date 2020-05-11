c0a6b0d4680363f9fb471a5ab6276b24f48d28b2
Leire Abarrategui Martinez <nla94@newcastle.ac.uk>
2020-05-11 16:45:33 +0100

    ds.ls documentation

--- R/ds.ls.R
+++ R/ds.ls.R
@@ -1,69 +1,120 @@
-#ds.ls
-#' @title lists all objects on a serverside environment
+#' @title lists all objects on a server-side environment
 #' @description creates a list of the names of all of the objects in
-#' a specified serverside environment
-#' @details Clientside function {ds.ls} calls serverside aggregate function
-#' {lsDS}. When running analyses one may want to know the objects already generated. This 
+#' a specified serverside environment. 
+#' @details When running analyses one may want to know the objects already generated. This 
 #' request is not disclosive as it only returns the names of the objects and not their contents. 
-#' By default, objects in DataSHIELD's 'active serverside analytic environment' (".GlobalEnv")
-#' will be listed. This
-#' is the environment that contains all of the objects that serverside DataSHIELD 
-#' is using for the main analysis or has written out to the serverside during the process
-#' of managing or undertaking the analysis (variables, scalars, matrices, data.frames etc). 
-#' The environment to explore is specified by the argument env.to.search (i.e. environment
+#' 
+#' By default, objects in DataSHIELD's Active Serverside Analytic Environment (\code{.GlobalEnv})
+#' will be listed. This is the environment that contains all of the objects that server-side DataSHIELD 
+#' is using for the main analysis or has written out to the server-side during the process
+#' of managing or undertaking the analysis (variables, scalars, matrices, data frames, etc).
+#'  
+#' The environment to explore is specified by the argument \code{env.to.search} (i.e. environment
 #' to search) to an integer value. The default environment 
-#' which R names as ".GlobalEnv" is set by specifying env.to.search = 1 or 1L
-#' (1L is just an explicit way of writing the integer 1). If the Boolean argument search.GlobalEnv
-#' is set to TRUE env.to.search is set to 1L regardless what value it is set in the call
-#' or if it is set to NULL. So if search.GlobalEnv is set to TRUE, ds.ls will automatically
-#' search the ".GlobalEnv" R environment on the serverside which contains all of the
-#' variables, data.frames and other objects read in at the start of the analysis,
-#' as well as any new objects of any sort 
-#' created using DataSHIELD assign functions. Other serverside environments contain other
-#' objects. For example, environment 2L contains the functions loaded via the native R
-#' stats package and 6L contains the standard list of datasets built into R. By default
-#' {ds.ls} will return a list of ALL of the objects in the environment specified by the
-#' env.to.search argument but you can specify search filters including '*' wildcards
-#' using the search.filter argument
-#' @param search.filter either NULL or a character string (potentially including '*'
-#' wildcards). Thus, search.filter="Sd2*" will list the names of all objects in the specified
-#' environment with names beginning capital S, lower case d and number 2. Similarly,
-#' search.filter="*.ID" will return all objects with names ending with ".ID" eg "Study.ID".
-#' If a value is not specified for the search.filter argument, or it is set as NULL, the names of 
+#' which R names as \code{.GlobalEnv} is set by specifying \code{env.to.search = 1} or \code{1L}
+#' (\code{1L} is just an explicit way of writing the integer \code{1}). 
+#' 
+#' If the \code{search.GlobalEnv} argument is set to TRUE the \code{env.to.search} parameter  
+#' is set to \code{1L} regardless of what value it is set in the call
+#' or if it is set to NULL. 
+#' So, if \code{search.GlobalEnv} is set to TRUE, \code{ds.ls} will automatically
+#' search the \code{.GlobalEnv} R environment on the server-side which contains all of the
+#' variables, data frames and other objects read in at the start of the analysis,
+#' as well as any new objects of any sort created using DataSHIELD assign functions. 
+#' 
+#' Other server-side environments contain other
+#' objects. For example, environment \code{2L} contains the functions loaded via the native R
+#' stats package and \code{6L} contains the standard list of datasets built into R. By default
+#' \code{ds.ls} will return a list of ALL of the objects in the environment specified by the
+#' \code{env.to.search} argument but you can specify search filters including \code{*} wildcards
+#' using the \code{search.filter} argument.
+#' 
+#' In \code{search.filter} you can use the symbol \code{*} to find all the object that contains
+#' the specified characters. For example, \code{search.filter = "Sd2*"} 
+#' will list the names of all objects in the specified
+#' environment with names beginning capital S, lower case d and number 2. 
+#' Similarly, \code{search.filter="*.ID"} will return all objects with names ending with \code{.ID},
+#' for example \code{Study.ID}. 
+#' If a value is not specified for the \code{search.filter} argument or it is set as NULL, the names of 
 #' all objects in the specified environment will be returned.
-#' @param env.to.search integer (e.g. in '2' or '2L' format) specifying the position
-#' in the search path of the environment to be explored. 1L is current active analytic
-#' environment on serverside and is the default value of env.to.search. Other environments
-#' can be explored instead by explicitly specifying the value of env.to.search in the
-#' clientside call (see details [above])
-#' @param search.GlobalEnv a Boolean function. If TRUE, {ds.ls} will list all objects
-#' in the .GlobalEnv R environment on the serverside. If FALSE and if env.to.search is also
-#' set as a valid integer, {ds.ls} will list all objects in the serverside R environment
-#' identified by pos=env.to.search in the search path. 
-#' @param datasources specifies the particular 'connection object(s)' to use.
-#' e.g. if you have several data sets in the sources you are working with
-#' called opals.a, opals.w2, and connection.xyz, you can choose which of
-#' these to work with. The call 'datashield.connections_find()' lists all of
-#' the different datasets available and if one of these is called 'default.connections'
-#' that will be the dataset used by default if no other dataset is specified. If you
-#' wish to change the connections you wish to use by default the call
-#' datashield.connections_default('opals.a') will set 'default.connections'
-#' to be 'opals.a' and so in the absence of specific instructions to the contrary
-#' (e.g. by specifiying a particular dataset to be used via the <datasources>
-#' argument) all subsequent function calls will be to the datasets held in opals.a.
-#' If the <datasources> argument is specified, it should be set without
-#' inverted commas: e.g. datasources=opals.a or datasources=default.connections.
-#' The <datasources> argument also allows you to apply a function solely to a subset
-#' of the studies/sources you are working with. For example, the second source
-#' in a set of three, can be specified using a call such as datasources=connection.xyz[2].
-#' On the other hand, if you wish to specify solely the first and third sources, the
-#' appropriate call will be datasources=connections.xyz[c(1,3)]
-#' @return a list containing: (1) the name/details of the serverside R environment
-#' which {ds.ls} has searched; (2) a vector of character strings giving the names of
-#' all objects meeting the naming criteria specified by the argument <search.filter> in this
-#' specified R serverside environment; (3) the nature of the search filter string as it was
-#' actually applied
-#' @author Gaye, A (2015). Updated and extended by Paul Burton (2020).
+#' 
+#' Server function called: \code{lsDS}. 
+#' 
+#' @param search.filter character string (potentially including \code{*} symbol) specifying the filter 
+#' for the object name that you want to find in the enviroment. For more information see \strong{Details}. 
+#' @param env.to.search an integer (e.g. in \code{2} or \code{2L} format) specifying the position
+#' in the search path of the environment to be explored. \code{1L} is the current active analytic
+#' environment on the server-side and is the default value of \code{env.to.search}.
+#' For more information see \strong{Details}.
+#' @param search.GlobalEnv Logical. If TRUE, \code{ds.ls} will list all objects
+#' in the \code{.GlobalEnv} R environment on the server-side. If FALSE and if \code{env.to.search} is also
+#' set as a valid integer, \code{ds.ls} will list all objects in the server-side R environment
+#' identified by \code{env.to.search} in the search path. 
+#' For more information see \strong{Details}.
+#' @param datasources a list of \code{\link{DSConnection-class}} 
+#' objects obtained after login. If the \code{datasources} argument is not specified
+#' the default set of connections will be used: see \code{\link{datashield.connections_default}}.
+#' @return \code{ds.ls} returns to the client-side a list containing: \cr
+#' (1) the name/details of the server-side R environment which \code{ds.ls} has searched;\cr
+#' (2) a vector of character strings giving the names of
+#' all objects meeting the naming criteria specified by the argument \code{search.filter} in this
+#' specified R server-side environment;\cr
+#' (3) the nature of the search filter string as it was applied. 
+#' @author DataSHIELD Development Team
+#' @examples
+#' \dontrun{
+#'
+#'   ## Version 6, for version 5 see the Wiki
+#'   
+#'   # connecting to the Opal servers
+#' 
+#'   require('DSI')
+#'   require('DSOpal')
+#'   require('dsBaseClient')
+#'
+#'   builder <- DSI::newDSLoginBuilder()
+#'   builder$append(server = "study1", 
+#'                  url = "http://192.168.56.100:8080/", 
+#'                  user = "administrator", password = "datashield_test&", 
+#'                  table = "CNSIM.CNSIM1", driver = "OpalDriver")
+#'   builder$append(server = "study2", 
+#'                  url = "http://192.168.56.100:8080/", 
+#'                  user = "administrator", password = "datashield_test&", 
+#'                  table = "CNSIM.CNSIM2", driver = "OpalDriver")
+#'   builder$append(server = "study3",
+#'                  url = "http://192.168.56.100:8080/", 
+#'                  user = "administrator", password = "datashield_test&", 
+#'                  table = "CNSIM.CNSIM3", driver = "OpalDriver")
+#'   logindata <- builder$build()
+#'   
+#'   connections <- DSI::datashield.login(logins = logindata, assign = TRUE, symbol = "D") 
+#'   
+#'   #Example 1: Obtain the list of  all objects on a server-side environment
+#'   
+#'   ds.ls(datasources = connections)
+#'   
+#'   #Example 2: Obtain the list of all objects that contain "var" character in the name
+#'   #Create in the server-side variables with "var" character in the name
+#'   
+#'   ds.assign(toAssign = "D$LAB_TSC",
+#'             newobj = "var.LAB_TSC",
+#'             datasources = connections)
+#'   ds.assign(toAssign = "D$LAB_TRIG",
+#'             newobj = "var.LAB_TRIG",
+#'             datasources = connections)
+#'   ds.assign(toAssign = "D$LAB_HDL",
+#'             newobj = "var.LAB_HDL",
+#'             datasources = connections)
+#'   
+#'   ds.ls(search.filter = "var*",
+#'         env.to.search = 1L,
+#'         search.GlobalEnv = TRUE,
+#'         datasources = connections)
+#'   
+#'   # clear the Datashield R sessions and logout
+#'   datashield.logout(connections)
+#' }
+#'
 #' @export
 ds.ls <- function(search.filter=NULL, env.to.search=1L, search.GlobalEnv=TRUE, datasources=NULL){
   
