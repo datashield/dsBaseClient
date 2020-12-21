99e45347e59a0eb7a25f06f849195d764027ac89
Stuart Wheater <stuart.wheater@arjuna.com>
2020-12-21 14:03:14 +0000

    Initial 'ds.metadata.R'

---
+++ R/ds.metadata.R
@@ -0,0 +1,72 @@
+#'
+#' @title Gets the metadata of an object in the server-side
+#' @description This function gets the metadata of an object
+#' stored on the server-side. 
+#' @details 
+#' Server function called: \code{metadataDS}
+#' @param x a character string specifying the name of the object.
+#' @param datasources a list of \code{\link{DSConnection-class}} 
+#' objects obtained after login. If the \code{datasources} argument is not specified
+#' the default set of connections will be used: see \code{\link{datashield.connections_default}}.
+#' @return \code{ds.metadata} returns to the client-side the metadata of associated to an object
+#' held at the server.
+#' @author Stuart Wheater, DataSHIELD Development Team
+#' @export
+#' @examples
+#' \dontrun{
+#'
+#'   # connecting to the Opal servers
+#' 
+#'   require('DSI')
+#'   require('DSOpal')
+#'   require('dsBaseClient')
+#'
+#'   builder <- DSI::newDSLoginBuilder()
+#'   builder$append(server = "study1", 
+#'                  url = "http://192.168.56.100:8080/", 
+#'                  user = "administrator", password = "datashield_test&", 
+#'                  table = "CNSIM.CNSIM1", driver = "OpalDriver")
+#'   builder$append(server = "study2", 
+#'                  url = "http://192.168.56.100:8080/", 
+#'                  user = "administrator", password = "datashield_test&", 
+#'                  table = "CNSIM.CNSIM2", driver = "OpalDriver")
+#'   builder$append(server = "study3",
+#'                  url = "http://192.168.56.100:8080/", 
+#'                  user = "administrator", password = "datashield_test&", 
+#'                  table = "CNSIM.CNSIM3", driver = "OpalDriver")
+#'   logindata <- builder$build()
+#'   
+#'   connections <- DSI::datashield.login(logins = logindata, assign = TRUE, symbol = "D") 
+#'   
+#'   # Example 1: Get the metadata associated with variable 'D'
+#'   ds.metadata(x = 'D$LAB_TSC', datasources = connections)
+#'
+#'   # clear the Datashield R sessions and logout
+#'   DSI::datashield.logout(connections)
+#' }
+#'
+ds.metadata = function(x=NULL, datasources=NULL)
+{
+
+    #####################################################################################
+    #MODULE 1: IDENTIFY DEFAULT CONNECTIONS                                             #
+    # look for DS connections                                                           #
+    if (is.null(datasources)){                                                          #
+        datasources <- datashield.connections_find()                                    #
+    }                                                                                   #
+    #####################################################################################
+
+    #####################################################################################
+    #MODULE 2: SET UP KEY VARIABLES ALLOWING FOR DIFFERENT INPUT FORMATS                #
+    if (is.null(x)){                                                                    #
+        stop("Please provide the name of the object!", call.=FALSE)                     #
+    }                                                                                   #
+    #####################################################################################
+
+    cally     <- call("metadataDS", x)
+    metadatas <- DSI::datashield.aggregate(datasources, cally)
+
+    return(metadatas)
+
+}
+#ds.metadata
