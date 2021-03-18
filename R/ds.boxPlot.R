e50e5865ee1ca6505b02312540060885db6b448f
Alex W <alex.westerberg@newcastle.ac.uk>
2021-03-18 12:17:22 +0000

    ds.boxPlot documentation ggplot example

--- R/ds.boxPlot.R
+++ R/ds.boxPlot.R
@@ -20,7 +20,8 @@
 #' \dontrun{
 #'   ## Version 6, for version 5 see the Wiki
 #'   
-#'   ### Please ensure you have a training Virtual Machine running, or that you have a live connection to a server.
+#'   ### Please ensure you have a training Virtual Machine running,
+#'    or that you have a live connection to a server.
 #'      
 #'   # Connecting to the Opal servers
 #' 
@@ -43,7 +44,8 @@
 #'                  table = "CNSIM.CNSIM3", driver = "OpalDriver")
 #'   logindata <- builder$build()
 #'   
-#'   connections <- DSI::datashield.login(logins = logindata, assign = TRUE, symbol = "D") 
+#'   connections <- DSI::datashield.login(logins = logindata, assign = TRUE,
+#'    symbol = "D") 
 #'   
 #'   ## Create a boxplot of one variable
 #'   ds.boxPlot("D", "LAB_HDL", datasources = connections) 
@@ -52,15 +54,29 @@
 #'   ds.boxPlot("D", "LAB_HDL", type= "split", datasources = connections) 
 #'   
 #'   ## Create a boxplot of two variables variable
-#'   ds.boxPlot("D", c("LAB_HDL", "LAB_TRIG", type="combine", datasources = connections)  # only one plot is created (of the aggregated results of all servers)
+#'   ds.boxPlot("D", c("LAB_HDL", "LAB_TRIG", type="combine", 
+#'   datasources = connections)  
+#'   # only one plot is created (of the aggregated results of all servers)
 #'   
 #'   ## Create a boxplot of two variables, which are split by a factor
-#'   ds.boxPlot("D", c("LAB_HDL", "LAB_TRIG"), group = "GENDER", datasources = connections)
+#'   ds.boxPlot("D", c("LAB_HDL", "LAB_TRIG"), group = "GENDER", 
+#'   datasources = connections)
 #'   
 #'   ## Create a boxplot with x- and y-axis labels
-#'   ds.boxPlot("D", c("LAB_HDL", "LAB_TRIG"), group = "GENDER", xlabel = "Variable", ylabel = "Measurement", datasources = connections)
+#'   ds.boxPlot("D", c("LAB_HDL", "LAB_TRIG"), group = "GENDER", 
+#'   xlabel = "Variable", ylabel = "Measurement", datasources = connections)
 #'   
-#'                               
+#'   ## Improve the presentation of ds.boxplot output using ggplot:
+#'   ### User must save the output, which is in a ggplot format already:
+#'   a <- ds.boxPlot("D", c("LAB_HDL", "LAB_TRIG"), group = "GENDER", 
+#'   xlabel = "Variable", ylabel = "Measurement", datasources = connections)
+#'                  
+#'   ### Then customise output "a" using ggplot tools:
+#'   a + ggplot2::scale_fill_discrete(name = "Gender", labels = c("Male", "Female"))
+#'   
+#'   ### Or use an alternative way, to maintain the aesthetics:
+#'   a + ggplot2::scale_fill_brewer(name = "Gender", labels = c("Male", "Female")) 
+#'                                                                                 
 #'   # Clear the Datashield R sessions and logout                 
 #'   datashield.logout(connections) 
 #'   
