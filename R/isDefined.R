3b0ca38019273c5ea6bb86135f9e3b2354a08d8d
davraam <avraam.demetris@gmail.com>
2021-08-07 10:14:37 +0300

    suggested replacement of isDefined

--- R/isDefined.R
+++ R/isDefined.R
@@ -1,39 +1,30 @@
 #'
-#' @title Checks if the objecs are defined in studies
+#' @title Checks if an object is defined in all studies
 #' @description This is an internal function.
 #' @details In DataSHIELD an object included in analysis must be defined (i.e. exists)
 #' in all the studies. If not the process should halt.
-#' @param datasources a list of \code{\link{DSConnection-class}} objects obtained after login. If the <datasources>
-#' the default set of connections will be used: see \link{datashield.connections_default}.
-#' @param obj a character vector, the name of the objects to look for.
+#' @param datasources a list of \code{\link{DSConnection-class}} objects obtained after login. 
+#' If the \code{datasources} argument is not specified, the default set of connections will be
+#' used: see \code{\link{datashield.connections_default}}.
+#' @param obj a character, the name of the object to look for.
 #' @keywords internal
-#' @return a boolean vector.
+#' @return a boolean value.
 #'
 isDefined <- function(datasources=NULL, obj=NULL){
 
-  stdnames <- names(datasources)
+  extractObj <- extract(obj)
 
-  inputnames <- c()
-  inputobj <- unlist(obj)
-  for(i in 1:length(inputobj )){
-    chnames <- extract(inputobj[i])
-    if(is.na(chnames[[1]])){
-      inputnames <- append(inputnames, chnames[[2]])
-    }else{
-      inputnames <- append(inputnames, chnames[[1]])
-    }
+  if(is.na(extractObj$holders)){
+    cally <- call('exists', extractObj$elements)
+    out <- DSI::datashield.aggregate(datasources, cally)
+  }else{
+    dfname <- as.name(extractObj$holders)
+    cally <- call('exists', extractObj$elements, dfname)
+    out <- DSI::datashield.aggregate(datasources, cally)
   }
-
-  myObjects <- inputnames
-  results <- c()
-  for(i in 1:length(myObjects)){
-    cally <- call('exists', myObjects[i])
-    x <- DSI::datashield.aggregate(datasources, cally)
-    results <- append(results, mean(unlist(x)))
-  }
-  if(mean(results) != 1){
-    idx <- which(results == FALSE)
-    stop("The input object(s) ", paste(myObjects[idx],collapse=", ")," is(are) not defined on one or more of the studies!", call.=FALSE)
+  
+  if(any(out==FALSE)){
+    stop("The input object ", obj, " is not defined in one or more of the studies!", call.=FALSE)
   }else{
     return(TRUE)
   }
