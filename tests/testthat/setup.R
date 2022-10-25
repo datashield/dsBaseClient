c0b76e95f781e7077c07d4b5a9470979c943ba70
Stuart Wheater <stuart.wheater@arjuna.com>
2022-10-25 14:37:14 +0100

    Install and include 'Molgenis' R packages

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -22,7 +22,7 @@ library(e1071)
 library(DescTools)
 
 library(DSOpal)
-# library(DSMolgenisArmadillo)
+library(DSMolgenisArmadillo)
 library(DSLite)
 
 source("dstest_functions/ds_expect_variables.R")
