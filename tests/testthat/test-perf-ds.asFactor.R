41189a0408171617344c347a8b43ed458dbb25f4
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2026-04-16 15:12:54 +0200

    fix: escaped dollar signs in perf tests

--- tests/testthat/test-perf-ds.asFactor.R
+++ tests/testthat/test-perf-ds.asFactor.R
@@ -18,7 +18,7 @@ test_that("performance", {
     .current.time <- .start.time
 
     while (difftime(.current.time, .start.time, units = "secs")[[1]] < .durationSec) {
-        ds.asFactor("D\$LAB_TSC", newobj.name="factor.newobj")
+        ds.asFactor("D$LAB_TSC", newobj.name="factor.newobj")
 
         .count <- .count + 1
         .current.time <- Sys.time()
--- tests/testthat/test-perf-ds.dmtC2S.R
+++ tests/testthat/test-perf-ds.dmtC2S.R
@@ -18,7 +18,7 @@ test_that("performance", {
     .current.time <- .start.time
 
     while (difftime(.current.time, .start.time, units = "secs")[[1]] < .durationSec) {
-        ds.dmtC2S(toAssign.text="D\$LAB_TSC + D\$LAB_TRIG", newobj="dmtc2s.newobj")
+        ds.dmtC2S(toAssign.text="D$LAB_TSC + D$LAB_TRIG", newobj="dmtc2s.newobj")
 
         .count <- .count + 1
         .current.time <- Sys.time()
