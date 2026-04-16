e045b5e75b4e3291ee56b645000d94b4dfda52f2
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2026-04-16 19:55:25 +0200

    test: add performance tests for batch 10 functions

---
+++ tests/testthat/test-perf-ds.tapply.R
@@ -0,0 +1,50 @@
+
+#
+# Set up
+#
+
+# context("ds.tapply::perf::setup")
+connect.studies.dataset.cnsim(list("LAB_TSC", "LAB_TRIG"))
+
+#
+# Tests
+#
+
+# context("ds.tapply::perf::0")
+test_that("performance", {
+    .durationSec  <- 30 # seconds
+    .count        <- 0
+    .start.time   <- Sys.time()
+    .current.time <- .start.time
+
+    while (difftime(.current.time, .start.time, units = "secs")[[1]] < .durationSec) {
+        ds.tapply(X.name="D$LAB_TSC", INDEX.names="D$LAB_TRIG", FUN.name="mean")
+
+        .count <- .count + 1
+        .current.time <- Sys.time()
+    }
+
+    .current.rate   <- .count / (difftime(.current.time, .start.time, units = "secs")[[1]])
+    .reference.rate <- perf.reference.rate("ds.tapply::perf::0")
+    if (any(length(.reference.rate) == 0) || any(is.null(.reference.rate))) {
+        print(paste("ds.tapply::perf::0 ", .current.rate, 0.5, 2.0))
+        perf.reference.save("ds.tapply::perf::0", .current.rate, 0.5, 2.0)
+    } else {
+        print(paste("ds.tapply::perf::0 ", format(.current.rate, digits = 8), ", ", format(100.0 * .current.rate / .reference.rate, digits = 4), "%", sep = ''))
+    }
+
+    .reference.rate            <- perf.reference.rate("ds.tapply::perf::0")
+    .reference.tolerance.lower <- perf.reference.tolerance.lower("ds.tapply::perf::0")
+    .reference.tolerance.upper <- perf.reference.tolerance.upper("ds.tapply::perf::0")
+
+    expect_gt(.current.rate, .reference.rate * .reference.tolerance.lower, label = "Observed rate", expected.label = "lower threshold on rate")
+    expect_lt(.current.rate, .reference.rate * .reference.tolerance.upper, label = "Observed rate", expected.label = "upper threshold on rate")
+})
+
+#
+# Done
+#
+
+# context("ds.tapply::perf::shutdown")
+disconnect.studies.dataset.cnsim()
+# context("ds.tapply::perf::done")
