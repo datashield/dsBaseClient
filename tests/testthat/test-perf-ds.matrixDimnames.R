5dff8ad35f6af54795d3870bbbb2ab8397a95358
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2026-04-16 14:50:03 +0200

    fix: remove third-party copyright from new tests

--- tests/testthat/test-perf-ds.matrixDimnames.R
+++ tests/testthat/test-perf-ds.matrixDimnames.R
@@ -1,12 +1,3 @@
-#-------------------------------------------------------------------------------
-# Copyright (c) 2024-2025 Arjuna Technologies, Newcastle upon Tyne. All rights reserved.
-#
-# This program and the accompanying materials
-# are made available under the terms of the GNU Public License v3.0.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#-------------------------------------------------------------------------------
 
 #
 # Set up
