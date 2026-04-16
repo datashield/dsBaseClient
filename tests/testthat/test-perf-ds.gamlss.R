672802a7478c8260ef53f3a4821a771339503899
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2026-04-16 15:13:05 +0200

    fix: escaped dollar signs in perf tests

--- tests/testthat/test-perf-ds.gamlss.R
+++ tests/testthat/test-perf-ds.gamlss.R
@@ -18,7 +18,7 @@ test_that("performance", {
     .current.time <- .start.time
 
     while (difftime(.current.time, .start.time, units = "secs")[[1]] < .durationSec) {
-        ds.gamlss(formula="D\$LAB_TSC~D\$LAB_TRIG", family="NO", newobj="gamlss.newobj")
+        ds.gamlss(formula="D$LAB_TSC~D$LAB_TRIG", family="NO", newobj="gamlss.newobj")
 
         .count <- .count + 1
         .current.time <- Sys.time()
