1c2bebf69cf16c323484cdc81d1da047666894d2
Stuart Wheater <stuart.wheater@arjuna.com>
2026-05-22 15:03:03 +0100

    Fixed use of 'useFancyQuotes' option

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -33,6 +33,7 @@ source("connection_to_datasets/init_testing_datasets.R")
 source("connection_to_datasets/init_studies_datasets.R")
 source("connection_to_datasets/init_discordant_datasets.R")
 source("connection_to_datasets/init_mediation_datasets.R")
-options(datashield.errors.print = FALSE)
+base::options(datashield.errors.print = FALSE)
+base::options(useFancyQuotes = FALSE)
 
 # context("setup - done")
