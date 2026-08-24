3ae1a250db130a37405446c86afd902f3bb3711c
Stuart Wheater <stuart.wheater@arjuna.com>
2026-08-24 15:06:02 +0100

    Perf updates and fixes

--- tests/testthat/test-perf-ds.dim.R
+++ tests/testthat/test-perf-ds.dim.R
@@ -19,7 +19,7 @@ connect.studies.dataset.cnsim(list("LAB_TSC", "LAB_TRIG"))
 # Tests
 #
 
-# context("ds.dim::perf::combine:0")
+# context("ds.dim::perf::0")
 test_that("combine - performance", {
     .durationSec  <- 30 # seconds
     .count        <- 0
@@ -34,17 +34,17 @@ test_that("combine - performance", {
     }
 
     .current.rate   <- .count / (difftime(.current.time, .start.time, units = "secs")[[1]])
-    .reference.rate <- perf.reference.rate("ds.dim::perf::combine:0")
+    .reference.rate <- perf.reference.rate("ds.dim::perf::0")
     if (any(length(.reference.rate) == 0) || any(is.null(.reference.rate))) {
-        print(paste("ds.dim::perf::combine:0 ", .current.rate, 0.5, 2.0))
-        perf.reference.save("ds.dim::perf::combine:0", .current.rate, 0.5, 2.0)
+        print(paste("ds.dim::perf::0 ", .current.rate, 0.5, 2.0))
+        perf.reference.save("ds.dim::perf::0", .current.rate, 0.5, 2.0)
     } else {
-        print(paste("ds.dim::perf::combine:0 ", format(.current.rate, digits = 8), ", ", format(100.0 * .current.rate / .reference.rate, digits = 4), "%", sep = ''))
+        print(paste("ds.dim::perf::0 ", format(.current.rate, digits = 8), ", ", format(100.0 * .current.rate / .reference.rate, digits = 4), "%", sep = ''))
     }
 
-    .reference.rate            <- perf.reference.rate("ds.dim::perf::combine:0")
-    .reference.tolerance.lower <- perf.reference.tolerance.lower("ds.dim::perf::combine:0")
-    .reference.tolerance.upper <- perf.reference.tolerance.upper("ds.dim::perf::combine:0")
+    .reference.rate            <- perf.reference.rate("ds.dim::perf::0")
+    .reference.tolerance.lower <- perf.reference.tolerance.lower("ds.dim::perf::0")
+    .reference.tolerance.upper <- perf.reference.tolerance.upper("ds.dim::perf::0")
 
     expect_gt(.current.rate, .reference.rate * .reference.tolerance.lower, label = "Observed rate", expected.label = "lower threshold on rate")
     expect_lt(.current.rate, .reference.rate * .reference.tolerance.upper, label = "Observed rate", expected.label = "upper threshold on rate")
