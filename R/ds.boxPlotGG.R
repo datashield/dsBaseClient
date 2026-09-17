75c48be4801f9d4b3503453b1573b8b154249265
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2026-09-17 13:17:55 +0200

    renamed variable to fix lint error

--- R/ds.boxPlotGG.R
+++ R/ds.boxPlotGG.R
@@ -26,35 +26,35 @@ ds.boxPlotGG <- function(x, group = NULL, group2 = NULL, xlabel = "x axis", ylab
   x_var <- lower <- upper <- ymin <- ymax <- middle <- fill <- NULL
   datasources <- .set_datasources(datasources)
 
-  pt <- datashield.aggregate(datasources, call("boxPlotGGDS", data_table.name=x, group=group, group2=group2))
+  plot_data <- datashield.aggregate(datasources, call("boxPlotGGDS", data_table.name=x, group=group, group2=group2))
 
   if(type == "pooled"){
     num_servers <- length(names(datasources))
-    pt_merged <- NULL
+    plot_data_merged <- NULL
     for(i in 1:num_servers){
-      pt_merged <- rbind(pt_merged, pt[[i]]$data)
+      plot_data_merged <- rbind(plot_data_merged, plot_data[[i]]$data)
     }
-    pt_merged <- data.table::data.table(pt_merged)
+    plot_data_merged <- data.table::data.table(plot_data_merged)
     if(!is.null(group) & is.null(group2)){
-      pt_merged <- computeWeightedMeans(pt_merged, 
+      plot_data_merged <- computeWeightedMeans(plot_data_merged, 
                         variables = c("ymin", "lower", "middle", "upper", "ymax"), 
                         weight = "n", 
                         by = c("group", "x"))
     }
     else if(!is.null(group) & !is.null(group2)){
-      pt_merged <- computeWeightedMeans(pt_merged, 
+      plot_data_merged <- computeWeightedMeans(plot_data_merged, 
                         variables = c("ymin", "lower", "middle", "upper", "ymax"), 
                         weight = "n", 
                         by = c("group", "group2", "x"))
     }
     else{
-      pt_merged <- computeWeightedMeans(pt_merged, 
+      plot_data_merged <- computeWeightedMeans(plot_data_merged, 
                         variables = c("ymin", "lower", "middle", "upper", "ymax"), 
                         weight = "n", 
                         by = c("x"))
     }
-    if(pt[[1]][[length(pt[[1]])]] == "single_group"){
-      plt <- ggplot2::ggplot(pt_merged) +
+    if(plot_data[[1]][[length(plot_data[[1]])]] == "single_group"){
+      plt <- ggplot2::ggplot(plot_data_merged) +
         ggplot2::geom_boxplot(stat = "identity", ggplot2::aes(x=x, lower=lower,
                                                               upper=upper, ymin=ymin,
                                                               ymax=ymax, middle=middle,
@@ -64,8 +64,8 @@ ds.boxPlotGG <- function(x, group = NULL, group2 = NULL, xlabel = "x axis", ylab
         ggplot2::ylab(ylabel) +
         ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, hjust = 1))
     }
-    else if(pt[[1]][[length(pt[[1]])]] == "double_group"){
-      plt <- ggplot2::ggplot(pt_merged) +
+    else if(plot_data[[1]][[length(plot_data[[1]])]] == "double_group"){
+      plt <- ggplot2::ggplot(plot_data_merged) +
         ggplot2::geom_boxplot(stat = "identity", ggplot2::aes(x=x, lower=lower,
                                                               upper=upper, ymin=ymin,
                                                               ymax=ymax, middle=middle,
@@ -77,7 +77,7 @@ ds.boxPlotGG <- function(x, group = NULL, group2 = NULL, xlabel = "x axis", ylab
         ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, hjust = 1))
     }
     else{
-      plt <- ggplot2::ggplot(pt_merged) +
+      plt <- ggplot2::ggplot(plot_data_merged) +
         ggplot2::geom_boxplot(stat = "identity", ggplot2::aes(x=x, lower=lower,
                                                               upper=upper, ymin=ymin,
                                                               ymax=ymax, middle=middle)) +
@@ -92,8 +92,8 @@ ds.boxPlotGG <- function(x, group = NULL, group2 = NULL, xlabel = "x axis", ylab
     num_servers <- length(names(datasources))
     plt <- NULL
     for(i in 1:num_servers){
-      if(pt[[i]][[length(pt[[i]])]] == "single_group"){
-        plt[[i]] <- ggplot2::ggplot(pt[[i]][[1]]) +
+      if(plot_data[[i]][[length(plot_data[[i]])]] == "single_group"){
+        plt[[i]] <- ggplot2::ggplot(plot_data[[i]][[1]]) +
           ggplot2::geom_boxplot(stat = "identity", ggplot2::aes(x=x, lower=lower,
                                                                 upper=upper, ymin=ymin,
                                                                 ymax=ymax, middle=middle,
@@ -104,8 +104,8 @@ ds.boxPlotGG <- function(x, group = NULL, group2 = NULL, xlabel = "x axis", ylab
           ggplot2::ggtitle(paste0("Server: ", names(datasources[i]))) +
           ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, hjust = 1))
       }
-      else if(pt[[i]][[length(pt[[i]])]] == "double_group"){
-        plt[[i]] <- ggplot2::ggplot(pt[[i]][[1]]) +
+      else if(plot_data[[i]][[length(plot_data[[i]])]] == "double_group"){
+        plt[[i]] <- ggplot2::ggplot(plot_data[[i]][[1]]) +
           ggplot2::geom_boxplot(stat = "identity", ggplot2::aes(x=x, lower=lower,
                                                                 upper=upper, ymin=ymin,
                                                                 ymax=ymax, middle=middle,
@@ -118,7 +118,7 @@ ds.boxPlotGG <- function(x, group = NULL, group2 = NULL, xlabel = "x axis", ylab
           ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, hjust = 1))
       }
       else{
-        plt[[i]] <- ggplot2::ggplot(pt[[i]][[1]]) +
+        plt[[i]] <- ggplot2::ggplot(plot_data[[i]][[1]]) +
           ggplot2::geom_boxplot(stat = "identity", ggplot2::aes(x=x, lower=lower,
                                                                 upper=upper, ymin=ymin,
                                                                 ymax=ymax, middle=middle)) +
